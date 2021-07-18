9dace41e355a92a843c61185d39f50bf357f208c
Thodoris Psallidas <theopsall@gmail.com>
2021-07-19 02:11:30 +0300

    update utils

--- deep_video_extraction/analyze_video.py
+++ deep_video_extraction/analyze_video.py
@@ -1,9 +1,13 @@
 import cv2
 import numpy as np
 
-def read(video):
+
+def read(video: str) -> np.ndarray:
     cap = cv2.VideoCapture(video)
-    fps = int(cap.get(cv2.CAP_PROP_FPS)) # frame per second for the current video in order to average the frames
+    try:
+        fps = int(cap.get(cv2.CAP_PROP_FPS))  # frame per second for the current video in order to average the frames
+    except ValueError:
+        assert f"Cannot convert video {video} fps to integer"
     # return a matrix shape (seconds, 1 )
     # print(fps)
     success = True
@@ -12,7 +16,7 @@ def read(video):
     tmp_frames = []
     while success:
         success, frame = cap.read()
-        if (count +1) % (fps+1) == 0:
+        if (count + 1) % (fps + 1) == 0:
 
             average_frames.append(np.average(tmp_frames, axis=0))
             tmp_frames = []
@@ -23,11 +27,10 @@ def read(video):
     return np.array(average_frames)
 
 
-
 def main():
     pass
 
 
 if __name__ == "__main__":
     average = read('videos/sample.mp4')
-    print(average.shape)
+    print(type(average))
--- deep_video_extraction/utils/utils.py
+++ deep_video_extraction/utils/utils.py
@@ -2,7 +2,42 @@ import os
 import pathlib
 import cv2
 
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mkv', 'webm'}
 
-def crawl_directory():
+def is_dir(directory: str)->bool:
+    return os.path.isdir(directory)
+
+def is_file():
     pass
 
+def is_video(video: str) -> bool:
+    pass
+
+
+
+def crawl_directory(directory: str) -> list:
+
+    if not is_dir(directory):
+        raise FileNotFoundError
+
+    tree = []
+    subdirs = [folder[0] for folder in os.walk(directory)]
+
+    for subdir in subdirs:
+        files = next(os.walk(subdir))[2]
+        for _file in files:
+            tree.append(os.path.join(subdir, _file))
+    return tree
+
+def read_video():
+    pass
+
+def allowed_file(filename:str):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def isolate_audio(path: str):
+    pass
+
+def get_spectrogram(audio):
+    pass
