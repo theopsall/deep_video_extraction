65ff72b238887a0e44aeccf7bcc11656d297a714
theopsall <theopsall@gmail.com>
2021-10-20 12:52:03 +0300

    finish dataLoader

--- deep_video_extraction/utils/dataset.py
+++ deep_video_extraction/utils/dataset.py
@@ -3,14 +3,23 @@ from deep_video_extraction.utils.utils import crawl_directory
 
 
 class VideoDataset(Dataset):
-    def __init__(self, video_dir) -> None:
+    def __init__(self, video_dir, transform=None) -> None:
         super().__init__()
         self.video_dir = video_dir
         self.videos = crawl_directory(video_dir)
+        self.transform = transform
+
+    def __str__(self):
+        print(f'Video DataLoader')
 
     def __len__(self):
         return len(self.videos)
 
     def __getitem__(self, index):
-        video = self.videos[index]
-        return video
+        frame_path = self.videos[index]
+        frame = np.array(Image.open(frame_path).convert('RGB'))
+
+        if self.transform is not None:
+            augmentations = self.transform(frame=frame)
+            frame = augmentations['frame']
+        return frame
